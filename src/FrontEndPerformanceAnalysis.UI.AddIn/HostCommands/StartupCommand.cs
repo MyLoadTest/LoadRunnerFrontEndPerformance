bd7614dd6a736532c9335ba932cd12300099fc7f
Vitalii Maklai <vitalii.maklai@gmail.com>
2015-06-24 00:30:56 -0400

    Project skeleton

---
+++ src/FrontEndPerformanceAnalysis.UI.AddIn/HostCommands/StartupCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ICSharpCode.Core;
+using MyLoadTest.LoadRunnerFrontEndPerformanceAnalysis.UI.AddIn.Properties;
+
+namespace MyLoadTest.LoadRunnerFrontEndPerformanceAnalysis.UI.AddIn.HostCommands
+{
+    /// <summary>
+    ///     Represents the command that is executed automatically when IDE starts.
+    /// </summary>
+    public sealed class StartupCommand : AbstractMenuCommand
+    {
+        #region Public Methods
+
+        /// <summary>
+        ///     Invokes the command.
+        /// </summary>
+        public override void Run()
+        {
+            ResourceService.RegisterNeutralImages(Resources.ResourceManager);
+        }
+
+        #endregion
+    }
+}
