2259d29cb8c4d79e04d43eb4ec4fe3a02664e863
Vitalii Maklai <vitalii.maklai@gmail.com>
2015-09-09 01:21:42 -0400

    Implemented Serialize method for HarRoot

--- src/FrontEndPerformanceAnalysis.UI.AddIn/Har/HarRoot.cs
+++ src/FrontEndPerformanceAnalysis.UI.AddIn/Har/HarRoot.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Linq;
+using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using Omnifactotum.Annotations;
 
 namespace MyLoadTest.LoadRunnerFrontEndPerformanceAnalysis.UI.AddIn.Har
 {
     [DataContract]
     internal sealed class HarRoot
     {
+        #region Constants and Fields
+
+        private static readonly DataContractJsonSerializer Serializer =
+            new DataContractJsonSerializer(typeof(HarRoot));
+
+        #endregion
+
         #region Public Properties
 
         [DataMember(Name = "log", IsRequired = true)]
@@ -14,6 +23,29 @@ namespace MyLoadTest.LoadRunnerFrontEndPerformanceAnalysis.UI.AddIn.Har
         {
             get;
             set;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Serialize([NotNull] Stream stream)
+        {
+            #region Argument Check
+
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            if (!stream.CanWrite)
+            {
+                throw new ArgumentException(@"The stream is not writable.", nameof(stream));
+            }
+
+            #endregion
+
+            Serializer.WriteObject(stream, this);
         }
 
         #endregion
